eed8b538d8f149446823d38830832d7eb2a7c1f0
CuerpoCasa <gabialberini733@gmail.com>
2021-09-08 16:56:08 -0300

    V 0.7 SPortafolio continuación popoups, no logro que aparezca la cadena de imagenes

--- app/app.js
+++ app/app.js
@@ -1,3 +1,6 @@
+function bodyScrollingToggle(){
+        document.body.classList.toggle("stop-scrolling");
+}
 
 (()  =>{
         const filterContainer = document.querySelector(".portfolio-filter"),
@@ -9,7 +12,7 @@
         closeBtn = popup.querySelector(".pp-close"),
         projectDetailsContainer = popup.querySelector(".pp-details"),
         projectDetailsBtn = popup.querySelector(".pp-project-details-btn");
-        let itemIndex, sliderIndex, screenshots;
+        let itemIndex, slideIndex, screenshots;
 
         /* filter portfolio items */
 
@@ -17,12 +20,11 @@
                 if(event.target.classList.contains("filter-item") && !event.target.classList.contains("active")) {
                         /* desactivate existing active "filter-item" */
                         filterContainer.querySelector(".active").classList.remove("outer-shadow", "active");
-                        /* activate new filter */
-                        event.target.classList.add("active", "outer-shadow"); 
                         /* activate new "filter item"*/
+                        event.target.classList.add("active", "outer-shadow"); 
                         const target = event.target.getAttribute("data-target");
-                        portfolioItems.forEach((item) => {
-                                if(target === item.getAttribute("data-category")) {
+                        portfolioItems.forEach((item) =>{
+                                if(target === item.getAttribute("data-category") || target === "all"){
                                         item.classList.remove("hide");
                                         item.classList.add("show");
                                 } 
@@ -34,4 +36,33 @@
                 } 
         })
 
+        portfolioItemsContainer.addEventListener("click", (event) =>{
+                if(event.target.closest(".portfolio-item-inner")){
+                        const portfolioItem = event.target.closest(".portfolio-item-inner").parentElement;
+                        /* get the portfolioItem index*/
+                        itemIndex = Array.from(portfolioItem.parentElement.children).indexOf(portfolioItem);
+                        screenshots = portfolioItems[itemIndex].querySelector(".portfolio-item-img img").getAttribute("data-screenshots");        
+                        /* convert screenshots into array */
+                        screenshots = screenshots.split(",");   
+                        slideIndex = 0;
+                        popupToggle();
+                        popupSlideshow();       
+                }
+        })
+
+        closeBtn.addEventListener("click", (event) =>{
+                popupToggle();
+        })
+
+        function popupToggle() {
+                popup.classList.toggle("open");
+                bodyScrollingToggle();
+        }
+
+        function popupSlideshow(){
+                const imgSrc = screenshots[slideIndex];
+                const popupImg = popup.querySelector(".pp-img");
+                /*activate loader until the popupImg loaded*/
+                popupImg.src=imgSrc;
+        }
 })();
